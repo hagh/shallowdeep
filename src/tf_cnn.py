ef06dd0c7346b9c32359e9eea575dcff389eae64
hagh <hoazari@microsoft.com>
2020-07-31 13:33:23 -0700

    added keras version of notebook

--- src/tf_cnn.py
+++ src/tf_cnn.py
@@ -3,7 +3,6 @@ import matplotlib.pyplot as plt
 import numpy as np
 import tensorflow as tf
 from tensorflow.python.framework import ops
-from tf_slim.layers import layers as _layers
 
 
 def create_placeholders(n_H0, n_W0, n_C0, n_y):
@@ -26,38 +25,12 @@ def create_placeholders(n_H0, n_W0, n_C0, n_y):
 
     return X, Y
 
-def initialize_parameters():
-    """
-    Initializes weight parameters to build a neural network with tensorflow. The shapes are:
-                        W1 : [4, 4, 3, 8]
-                        W2 : [2, 2, 8, 16]
-    Returns:
-    parameters -- a dictionary of tensors containing W1, W2
-    """
-
-    tf.compat.v1.set_random_seed(1) # so that your "random" numbers match ours
-
-    W1 = tf.compat.v1.get_variable(
-        "W1",
-        [4,4,3,8],
-        initializer=tf.compat.v1.keras.initializers.VarianceScaling(scale=1.0, mode="fan_avg", distribution="uniform", seed=0))
-
-    W2 = tf.compat.v1.get_variable(
-        "W2",
-        [2,2,8,16],
-        initializer=tf.compat.v1.keras.initializers.VarianceScaling(scale=1.0, mode="fan_avg", distribution="uniform", seed=0))
-
-    parameters = {"W1": W1,
-                  "W2": W2}
-    
-    return parameters
-
 
 def forward_propagation(X, parameters):
     """
     Implements the forward propagation for the model:
     CONV2D -> RELU -> MAXPOOL -> CONV2D -> RELU -> MAXPOOL -> FLATTEN -> FULLYCONNECTED
-    
+
     Arguments:
     X -- input dataset placeholder, of shape (input size, number of examples)
     parameters -- python dictionary containing your parameters "W1", "W2"
@@ -66,35 +39,34 @@ def forward_propagation(X, parameters):
     Returns:
     Z3 -- the output of the last LINEAR unit
     """
-
-    # Retrieve the parameters from the dictionary "parameters" 
-    W1 = parameters['W1']
-    W2 = parameters['W2']
-
     # CONV2D: stride of 1, padding 'SAME'
-    Z1 = tf.nn.conv2d(input=X, filters=W1, strides=[1,1,1,1], padding='SAME')
+    #Z1 = tf.nn.conv2d(input=X, filters=W1, strides=[1, 1, 1, 1], padding='SAME')
+    Z1 = tf.compat.v1.layers.conv2d(X, 8, [4, 4], padding='SAME')
 
     # RELU
     A1 = tf.nn.relu(Z1)
 
     # MAXPOOL: window 8x8, sride 8, padding 'SAME'
-    P1 = tf.nn.max_pool2d(A1, ksize=[1,8,8,1], strides=[1,8,8,1], padding='SAME')
+    P1 = tf.nn.max_pool2d(A1, ksize=[1, 8, 8, 1], strides=[1, 8, 8, 1], padding='SAME')
 
     # CONV2D: filters W2, stride 1, padding 'SAME'
-    Z2 = tf.nn.conv2d(input=P1,filters=W2, strides=[1,1,1,1], padding='SAME')
+    #Z2 = tf.nn.conv2d(input=P1, filters=W2, strides=[1, 1, 1, 1], padding='SAME')
+    Z2 = tf.compat.v1.layers.conv2d(P1, 16, [2, 2], padding='SAME')
 
     # RELU
     A2 = tf.nn.relu(Z2)
 
     # MAXPOOL: window 4x4, stride 4, padding 'SAME'
-    P2 = tf.nn.max_pool2d(A2, ksize=[1,4,4,1], strides=[1,4,4,1], padding='SAME')
+    P2 = tf.nn.max_pool2d(A2, ksize=[1, 4, 4, 1], strides=[1, 4, 4, 1], padding='SAME')
 
     # FLATTEN
-    P2 = _layers.flatten(P2)
+    #P2 = _layers.flatten(P2)
+    P2 = tf.compat.v1.layers.flatten(P2)
 
     # FULLY-CONNECTED without non-linear activation function (not not call softmax).
-    # 6 neurons in output layer. Hint: one of the arguments should be "activation_fn=None" 
-    Z3 = _layers.fully_connected(P2, 2, activation_fn=None)
+    # 6 neurons in output layer. Hint: one of the arguments should be "activation_fn=None"
+    #Z3 = _layers.fully_connected(P2, 2, activation_fn=None)
+    Z3 = tf.compat.v1.layers.dense(P2, 2, activation=None)
 
     return Z3
 
@@ -112,7 +84,7 @@ def compute_cost(Z3, Y):
     """
 
     cost = tf.reduce_mean(input_tensor=tf.nn.softmax_cross_entropy_with_logits(logits=Z3, labels=tf.stop_gradient(Y)))
-    
+
     return cost
 
 
@@ -121,7 +93,7 @@ def model(X_train, Y_train, X_test, Y_test, learning_rate=0.009,
     """
     Implements a three-layer ConvNet in Tensorflow:
     CONV2D -> RELU -> MAXPOOL -> CONV2D -> RELU -> MAXPOOL -> FLATTEN -> FULLYCONNECTED
-    
+
     Arguments:
     X_train -- training set, of shape (None, 64, 64, 3)
     Y_train -- test set, of shape (None, n_y = 6)
@@ -131,80 +103,40 @@ def model(X_train, Y_train, X_test, Y_test, learning_rate=0.009,
     num_epochs -- number of epochs of the optimization loop
     minibatch_size -- size of a minibatch
     print_cost -- True to print the cost every 100 epochs
-    
+
     Returns:
     train_accuracy -- real number, accuracy on the train set (X_train)
     test_accuracy -- real number, testing accuracy on the test set (X_test)
     parameters -- parameters learnt by the model. They can then be used to predict.
     """
 
-    ops.reset_default_graph()                         # to be able to rerun the model without overwriting tf variables
-    tf.compat.v1.set_random_seed(1)                             # to keep results consistent (tensorflow seed)
-    seed = 3                                          # to keep results consistent (numpy seed)
-    (m, n_H0, n_W0, n_C0) = X_train.shape             
-    n_y = Y_train.shape[1]                            
-    costs = []                                        # To keep track of the cost
+    tf.compat.v1.set_random_seed(1)  # to keep results consistent (tensorflow seed)
+    seed = 3  # to keep results consistent (numpy seed)
+    (m, n_H0, n_W0, n_C0) = X_train.shape
+    n_y = Y_train.shape[1]
+    costs = []  # To keep track of the cost
 
-    # Create Placeholders of the correct shape
-    X, Y = create_placeholders(n_H0, n_W0, n_C0, n_y)
+    with tf.compat.v1.Graph().as_default() as graph:
+        # Create Placeholders of the correct shape
+        X, Y = create_placeholders(n_H0, n_W0, n_C0, n_y)
 
-    # Initialize parameters
-    parameters = initialize_parameters()
+        # Initialize parameters
+        parameters = None
 
-    # Forward propagation: Build the forward propagation in the tensorflow graph
-    Z3 = forward_propagation(X, parameters)
+        # Forward propagation: Build the forward propagation in the tensorflow graph
+        Z3 = forward_propagation(X, parameters)
 
-    # Cost function: Add cost function to tensorflow graph
-    cost = compute_cost(Z3, Y)
+        # Cost function: Add cost function to tensorflow graph
+        cost = compute_cost(Z3, Y)
 
-    # Backpropagation: Define the tensorflow optimizer. Use an AdamOptimizer that minimizes the cost.
-    optimizer = tf.compat.v1.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost)
+        # Backpropagation: Define the tensorflow optimizer. Use an AdamOptimizer that minimizes the cost.
+        optimizer = tf.compat.v1.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost)
 
-    # Get all variables
-    model_vars = tf.compat.v1.get_collection(tf.compat.v1.GraphKeys.GLOBAL_VARIABLES)
+        # Create a saver.
+        saver = tf.compat.v1.train.Saver()
 
-    # Create a saver.
-    saver = tf.compat.v1.train.Saver(model_vars)
-
-    # Initialize all the variables globally
-    init = tf.compat.v1.global_variables_initializer()
-
-    # Start the session to compute the tensorflow graph
-    with tf.compat.v1.Session() as sess:
-
-        # Run the initialization
-        sess.run(init)
-
-        # Do the training loop
-        for epoch in range(num_epochs):
-
-            minibatch_cost = 0.
-            num_minibatches = int(m / minibatch_size) # number of minibatches of size minibatch_size in the train set
-            seed = seed + 1
-            minibatches = cnn_utils.random_mini_batches(X_train, Y_train, minibatch_size, seed)
-
-            for minibatch in minibatches:
-
-                # Select a minibatch
-                (minibatch_X, minibatch_Y) = minibatch
-                # IMPORTANT: The line that runs the graph on a minibatch.
-                # Run the session to execute the optimizer and the cost, the feedict should contain a minibatch for (X,Y).
-                _ , temp_cost = sess.run([optimizer, cost], feed_dict={X: minibatch_X, Y: minibatch_Y})
-
-                minibatch_cost += temp_cost / num_minibatches
-
-            # Print the cost every epoch
-            if print_cost == True and epoch % 5 == 0:
-                print ("Cost after epoch %i: %f" % (epoch, minibatch_cost))
-            if print_cost == True and epoch % 1 == 0:
-                costs.append(minibatch_cost)
-
-            save_path = saver.save(sess, 'saves\my-model')
-
-        # Save session
-        for v in model_vars:
-            print(v)
-
+        # Initialize all the variables globally
+        init = tf.compat.v1.global_variables_initializer()
 
         # Calculate the correct predictions
         predict_op = tf.argmax(input=Z3, axis=1)
@@ -212,6 +144,37 @@ def model(X_train, Y_train, X_test, Y_test, learning_rate=0.009,
 
         # Calculate accuracy on the test set
         accuracy = tf.reduce_mean(input_tensor=tf.cast(correct_prediction, "float"))
+
+    # Start the session to compute the tensorflow graph
+    with tf.compat.v1.Session(graph=graph) as sess:
+        # Run the initialization
+        sess.run(init)
+
+        # Do the training loop
+        for epoch in range(num_epochs):
+
+            minibatch_cost = 0.
+            num_minibatches = int(m / minibatch_size)  # number of minibatches of size minibatch_size in the train set
+            seed = seed + 1
+            minibatches = cnn_utils.random_mini_batches(X_train, Y_train, minibatch_size, seed)
+
+            for minibatch in minibatches:
+                # Select a minibatch
+                (minibatch_X, minibatch_Y) = minibatch
+                # IMPORTANT: The line that runs the graph on a minibatch.
+                # Run the session to execute the optimizer and the cost, the feedict should contain a minibatch for (X,Y).
+                _, temp_cost = sess.run([optimizer, cost], feed_dict={X: minibatch_X, Y: minibatch_Y})
+
+                minibatch_cost += temp_cost / num_minibatches
+
+            # Print the cost every epoch
+            if print_cost == True and epoch % 5 == 0:
+                print("Cost after epoch %i: %f" % (epoch, minibatch_cost))
+            if print_cost == True and epoch % 1 == 0:
+                costs.append(minibatch_cost)
+
+        save_path = saver.save(sess, 'saves\my-model')
+
         print(accuracy)
         train_accuracy = accuracy.eval({X: X_train, Y: Y_train})
         test_accuracy = accuracy.eval({X: X_test, Y: Y_test})
@@ -223,47 +186,51 @@ def model(X_train, Y_train, X_test, Y_test, learning_rate=0.009,
         plt.ylabel('cost')
         plt.xlabel('iterations (per tens)')
         plt.title(
-            "Learning rate = {0}, Train Accuracy: {1}, Test Accuracy: {2}.".format(learning_rate, train_accuracy, test_accuracy))
+            "Learning rate = {0}, Train Accuracy: {1}, Test Accuracy: {2}.".format(learning_rate, train_accuracy,
+                                                                                   test_accuracy))
         plt.show()
 
         return train_accuracy, test_accuracy, parameters, save_path
 
 
 def predict(X, parameters, save_path):
-
     (m, n_H0, n_W0, n_C0) = X.shape
 
-    # Create Placeholders of the correct shape
-    x = tf.compat.v1.placeholder(tf.float32, shape=(None, n_H0, n_W0, n_C0), name="x")
+    with tf.compat.v1.Graph().as_default() as graph:
+        # Create Placeholders of the correct shape
+        x = tf.compat.v1.placeholder(tf.float32, shape=(None, n_H0, n_W0, n_C0), name="x")
 
-    z3 = forward_propagation(x, parameters)
-    p = tf.argmax(input=z3, axis=1)
+        z3 = forward_propagation(x, parameters)
+        p = tf.argmax(input=z3, axis=1)
 
-    # Create a saver.
-    saver = tf.compat.v1.train.Saver()
+        # Create a saver.
+        saver = tf.compat.v1.train.Saver()
 
-    # Initialize all the variables globally
-    #init = tf.compat.v1.global_variables_initializer()
+    with tf.compat.v1.Session(graph=graph) as sess:
+        # Restore the saved model
+        saver.restore(sess, save_path)
 
-    sess = tf.compat.v1.Session()
-
-    # Run the initialization
-    #sess.run(init)
-
-    # Restore the saved model
-    saver.restore(sess, save_path)
-
-    # Predict
-    prediction = sess.run(p, feed_dict={x: X})
+        # Predict
+        prediction = sess.run(p, feed_dict={x: X})
 
     return prediction
 
 
 def run_model(
-    X_train, Y_train, X_test, Y_test,
-    learning_rate=0.009,
-    num_epochs=100, minibatch_size=64, print_cost=True):
-
-    _, _, parameters, save_path = model(X_train, Y_train, X_test, Y_test, learning_rate, num_epochs, minibatch_size, print_cost)
+        X_train, Y_train, X_test, Y_test,
+        learning_rate=0.009,
+        num_epochs=100, minibatch_size=64, print_cost=True):
+    _, _, parameters, save_path = model(X_train, Y_train, X_test, Y_test, learning_rate, num_epochs, minibatch_size,
+                                        print_cost)
 
     return parameters, save_path
+
+
+if __name__ == '__main__':
+    x_train = np.zeros((50, 300, 300, 3))
+    y_train = np.zeros((50, 2))
+    x_test = np.zeros((10, 300, 300, 3))
+    y_test = np.zeros((10, 2))
+
+    params, save_path = run_model(x_train, y_train, x_test, y_test, num_epochs=0)
+    predict(x_test, params, save_path)
